59b4a69811e66be911bd9bb702fb26c2a29f4704
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2024-04-23 23:44:53 +0200

    done

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++
@@ -1,8 +0,0 @@
-export default function iterateThroughObject(reportWithIterator) {
-  let employees = '';
-  for (const item of reportWithIterator) {
-    employees += `${item} | `;
-  }
-  return employees.slice(0, -3);
-}
-
